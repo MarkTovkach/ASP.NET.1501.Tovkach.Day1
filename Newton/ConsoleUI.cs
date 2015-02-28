8bef779eb80faaba4c4f52ae3c24f5b882e463b6
MarkTovkach <marktovkach@gmail.com>
2015-02-28 16:58:16 +0300

    1

---
+++ Newton/ConsoleUI.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton
+{
+    class ConsoleUI
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber;
+            double indexNumber;
+            double accuracy;
+            double accuracyOfComparison;
+
+            Console.WriteLine("Please input the base:");
+            string s = Console.ReadLine();
+            while (Double.TryParse(s, out baseNumber) != true)
+            {
+                Console.WriteLine("Wrong input.Try again:");
+                s = Console.ReadLine();
+            }
+            baseNumber = Convert.ToDouble(s);
+
+            Console.WriteLine("Please input the index:");
+            s = Console.ReadLine();
+            while (Double.TryParse(s, out indexNumber) != true)
+            {
+                Console.WriteLine("Wrong input.Try again:");
+                s = Console.ReadLine();
+            }
+            indexNumber = Convert.ToDouble(s);
+            Console.WriteLine("Please input the accuracy:");
+            s = Console.ReadLine();
+            while (Double.TryParse(s, out accuracy) != true)
+            {
+                Console.WriteLine("Wrong input.Try again:");
+                s = Console.ReadLine();
+            }
+            accuracy = Convert.ToDouble(s);
+
+            Console.WriteLine("Your result:{0}", NewtonMethod.RootOfNDegree(baseNumber, indexNumber, accuracy));
+            Console.WriteLine("Do you want to compare the result with true value??? Y/N");
+            string c = Console.ReadLine();
+            char j = Convert.ToChar(c);
+            if (j == 89 || j == 121)
+            {
+                Console.WriteLine("Please input the accuracy for compare:");
+                string k = Console.ReadLine();
+                while (Double.TryParse(k, out accuracyOfComparison) != true)
+                {
+                    Console.WriteLine("Wrong input.Try again:");
+                    k = Console.ReadLine();
+                }
+                accuracyOfComparison = Convert.ToDouble(k);
+                if (Math.Abs(baseNumber - Math.Pow(NewtonMethod.RootOfNDegree(baseNumber, indexNumber, accuracy), indexNumber)) < accuracyOfComparison)
+                {
+                    Console.WriteLine("After comparing your result is true with this accuracy.");
+                    Console.ReadKey();
+                }
+                else
+                {
+                    Console.WriteLine("After comparing your result is false with this accuracy. ");
+                    Console.ReadKey();
+
+                }
+            }
+            else
+            {
+                if (j == 78 || j == 110)
+                {
+                    Console.WriteLine("Thank you.");
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
---
+++ Newton/NewtonMethod.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Newton
+{
+    class NewtonMethod
+    {
+        /// <summary>
+        /// The method returns a value equal to the root of n-th power of the number with a specified accuracy. 
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="n"></param>
+        /// <param name="eps"></param>
+        /// <returns></returns>
+        public static double RootOfNDegree(double x, double n, double eps)
+        {
+            double x1;
+            double x0 = x / 2;
+            double a = x;
+            do
+            {
+                x1 = x0;
+                x = (1 / n) * ((n - 1) * x1 + a / Math.Pow(x1,(n-1)));
+                x0 = x;
+
+            } while (Math.Abs(x0 - x1) > eps);
+            return (x);
+        }
+    }
+}
