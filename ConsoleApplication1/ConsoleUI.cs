0ff6d8d475485ad7201755f68f68419742b25359
MarkTovkach <marktovkach@gmail.com>
2015-03-01 05:29:27 +0300

    Sorting

---
+++ ConsoleApplication1/ConsoleUI.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    class ConsoleUI
+    {
+        static void Main(string[] args)
+        {
+            int size,size2,element, key;
+            bool order = true;
+            //The Initialization of a Ragged Array 
+            Console.WriteLine("Hi! Please, input the array size: ");
+            string s = Console.ReadLine();
+
+            while (Int32.TryParse(s, out size) != true)
+            {
+                Console.WriteLine("Wrong input.Try again:");
+                s = Console.ReadLine();
+
+            }
+            size = Int32.Parse(s);
+  
+
+            int[][] jaggedArray = new int[size][];
+
+            for (int i = 0; i < jaggedArray.Length; i++)
+            {
+                Console.WriteLine("Input the {0}th array size: ", i);
+                 s = Console.ReadLine();
+
+                while (Int32.TryParse(s, out size) != true)
+                {
+                    Console.WriteLine("Wrong input.Try again:");
+                    s = Console.ReadLine();
+
+                }
+                size2 = Int32.Parse(s);
+                jaggedArray[i] = new int[size2];
+            }
+
+            //Filling an array
+            for(int i=0;i<jaggedArray.Length;i++)
+                for(int j=0;j<jaggedArray[i].Length;j++)
+                {
+                    Console.WriteLine("Input [{0}][{1}]",i,j);
+                    s = Console.ReadLine();
+
+                   while (Int32.TryParse(s, out element) != true)
+                    {
+                        Console.WriteLine("Wrong input.Try again:");
+                        s = Console.ReadLine();
+
+                    }
+
+                    element = Int32.Parse(s);
+                    jaggedArray[i][j]=element;
+
+                 }
+            // Select the order of sorting
+
+            Console.WriteLine("Ascending or descending order A/D");
+            string k = Console.ReadLine();
+            if (k == "A" || k == "a")
+                order = true;
+            else if (k == "D" || k == "d")
+                order = false;
+            // Select sorting feature
+            Console.WriteLine("On the amounts of the elements in rows(Input 1)/On the min of the elements in rows(Input 2)/On the max of the elements in rows(Input 3)");
+            k = Console.ReadLine();
+            int position = Convert.ToInt32(k);
+
+            switch (position)
+            {
+                case 1: SortingRows.SortByMax(jaggedArray, order);
+                    break;
+                case 2: SortingRows.SortByMin(jaggedArray, order);
+                    break;
+                case 3: SortingRows.SortByMax(jaggedArray, order);
+                    break;
+            }
+
+        }
+    }
+}
---
+++ ConsoleApplication1/SortingRows.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sorting
+{
+    public static class SortingRows
+    {
+        public static int[][] BubbleSort( int[][] array, int[] arrayForSort, bool course)
+        {
+
+            for (int i = 0; i < arrayForSort.Length; i++)
+            {
+                for (int j = i + 1; j < arrayForSort.Length; j++)
+                {
+                    if (course)
+                    {
+                        if (arrayForSort[j] < arrayForSort[i])
+                        {
+                            Swap(ref arrayForSort[j], ref arrayForSort[i]);
+                            Swap(ref array[j], ref array[i]);
+                        }
+                    }
+                    else
+                    {
+                        if (arrayForSort[j] > arrayForSort[i])
+                        {
+                            Swap(ref arrayForSort[j], ref arrayForSort[i]);
+                            Swap(ref array[j], ref array[i]);
+                        }
+                    }
+                }
+            }
+            return array;
+        }
+        public static void Swap(ref int firstElement, ref int secondElement)
+        {
+            int temp = firstElement;
+            firstElement = secondElement;
+            secondElement = temp;
+        }
+        public static int[][] SortBySum(int[][] array, bool course)
+       {
+           int[] arrayForSort = new int[array.Length];
+           for (int i = 0; i < array.Length; i++)
+           {
+               arrayForSort[i] = array[i].Sum();
+           }
+           BubbleSort(ref int [][]array, int []arrayForSort, course);
+           return array;
+       }
+        public static int[][] SortByMax(int[][] array, bool course) 
+       {
+           int[] arrayForSort = new int[array.Length];
+           for (int i = 0; i < array.Length; i++)
+           {
+               arrayForSort[i] = array[i].Max();
+            }
+           BubbleSort(ref int [][]array, int []arrayForSort, course);
+           return array;
+       }
+
+        public static int[][] SortByMin(int[][] array, bool course) 
+       {
+           int[] arrayForSort = new int[array.Length];
+           for (int i = 0; i < array.Length; i++)
+           {
+               arrayForSort[i] = array[i].Min();
+            }
+           BubbleSort(ref int [][]array, int []arrayForSort, course);
+           return array;
+       }
+
+    }
+}
